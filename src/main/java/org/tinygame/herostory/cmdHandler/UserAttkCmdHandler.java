7a4749dc15d6c5dbe212f28813965c1c4717b426
changmingke <changmingke@artspring.com.cn>
2020-03-01 22:57:54 +0800

    增加用户攻击效果处理器

---
+++ src/main/java/org/tinygame/herostory/cmdHandler/UserAttkCmdHandler.java
@@ -0,0 +1,49 @@
+package org.tinygame.herostory.cmdHandler;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.AttributeKey;
+import org.tinygame.herostory.Broadcaster;
+import org.tinygame.herostory.msg.GameMsgProtocol;
+
+/**
+ * 用户攻击信息处理器
+ *
+ * @auther changmk
+ * @date 2020/3/1 下午10:53
+ */
+public class UserAttkCmdHandler implements ICmdHandler<GameMsgProtocol.UserAttkCmd> {
+
+    @Override
+    public void handle(ChannelHandlerContext ctx, GameMsgProtocol.UserAttkCmd msg) {
+        if (ctx == null || msg == null) {
+            return;
+        }
+
+        // 获取攻击者 Id
+        Integer attkUserId = (Integer) ctx.channel().attr(AttributeKey.valueOf("userId")).get();
+        if (null == attkUserId) {
+            return;
+        }
+
+        // 获取被攻击者 Id
+        int targetUserId = msg.getTargetUserId();
+
+        GameMsgProtocol.UserAttkResult.Builder resultBuilder = GameMsgProtocol.UserAttkResult.newBuilder();
+        resultBuilder.setAttkUserId(attkUserId);
+        resultBuilder.setTargetUserId(targetUserId);
+
+        GameMsgProtocol.UserAttkResult newResult = resultBuilder.build();
+        Broadcaster.broadcast(newResult);
+
+        // 减血消息, 可以根据自己的喜好写...
+        // 例如加上装备加成, 暴击等等.
+        // 这些都属于游戏的业务逻辑了!
+        GameMsgProtocol.UserSubtractHpResult.Builder resultBuilder2 = GameMsgProtocol.UserSubtractHpResult.newBuilder();
+        resultBuilder2.setTargetUserId(targetUserId);
+        resultBuilder2.setSubtractHp(10);
+
+        GameMsgProtocol.UserSubtractHpResult newResult2 = resultBuilder2.build();
+        Broadcaster.broadcast(newResult2);
+
+    }
+}
